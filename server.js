c8f05509567a310602347083dce02ad0116278ec
Aminakzyzz <121366309+Aminakzyzz@users.noreply.github.com>
2023-05-20 22:13:07 +0200

    Updated bot.js with "most matched keywords"
    Updated bot.js with "most matched keywords"

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+
+
 import { Server } from 'socket.io';
 import express from 'express';
 import { createServer } from 'http';
-import { testKeywordSpotting } from './bot.js';
+import { processUserInput } from './bot.js';
 
-const app = express(); 
-const server = createServer(app); 
+const app = express();
+const server = createServer(app);
 const io = new Server(server);
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.static('public'));//displays the index.html file in the public folder
+app.use(express.static('public')); // Displays the index.html file in the public folder
 
 io.on('connection', socket => {
+  socket.emit('start');
 
-	socket.emit('start');	
+  socket.on('clientRequest', message => {
+    const responseText = processUserInput(message);
+    socket.emit('serverResponse', responseText); // Send the response back to the client
+  });
+});
 
-	socket.on('clientRequest', async message => {
-		let responseText = testKeywordSpotting(message);
-		socket.emit('serverResponse',responseText );
-	  });
-	
-  });	
 server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+
