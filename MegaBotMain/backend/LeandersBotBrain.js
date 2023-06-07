980a2cfcbb708cbf0a6f98764cdbcf899c34f31e
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2023-06-07 22:09:55 +0200

    New Look Folder + some changes

--- MegaBotMain/backend/LeandersBotBrain.js
+++ MegaBotMain/backend/LeandersBotBrain.js
@@ -6,6 +6,11 @@ var current_node = parsed["start"];
 
 
 
+var correctCounter = 0;
+var falseCounter = 0;
+var TEST_MODE = true;
+// var lineCount = 20
+
 // Function to find the matching intent for a given message
 function processUserInput(userInput) {
 
@@ -16,14 +21,37 @@ function processUserInput(userInput) {
     }else{
       var response = "";
 
-      if(current_node.correct_answers.includes(userInput)){
+      var userGotItCorrect = false;
+
+      current_node.correct_answers.forEach(correct_answer => {
+
+        if(userInput.includes(correct_answer)){
+          userGotItCorrect = true;
+        }
+        // console.log(correct_answer);
+      });
+      
+
+      if(userGotItCorrect){
         response = current_node.feedback_correct;
+        correctCounter++;
       }else{
         response = current_node.feedback_incorrect;
+        falseCounter++;
       }
 
-      current_node = parsed[current_node.next_node];
-      response += "\n" + current_node.text;
+
+
+      if(current_node.next_node != undefined){
+ current_node = parsed[current_node.next_node];
+  response += "\n" + current_node.text;
+      }else{
+        //END!!!!!!!!!!
+//  response += "Out of ${20} questions \n you got ${correctCounter} correct \n and ${falseCounter} false answers. :)";
+ response += `Out of ${20} questions \n you got ${correctCounter} correct \n and ${falseCounter} false answers. :)`;
+      }
+     
+     
       return response;
     }
    
